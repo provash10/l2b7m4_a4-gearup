4a53d3a1ecd8274dec2afedafd6c5c217d575e51
Provash <provash20cb@gmail.com>
2026-07-08 13:24:23 +0600

    app.ts and server.ts creating by express

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
+
+app.get("/",(req: Request, res: Response)=>{
+    res.send("GearUp is active !!!")
+})
+
+export default app;
