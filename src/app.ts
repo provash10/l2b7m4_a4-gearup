0e0281c38a65cf0cb4030e05e62ec4385fddc8c6
Provash <provash20cb@gmail.com>
2026-07-08 21:11:50 +0600

    config env and express middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import config from "./config";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
+
+app.use(cors({
+    origin : config.app_url,
+    credentials : true
+}));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
 
 app.get("/",(req: Request, res: Response)=>{
     res.send("GearUp is active !!!")
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,23 @@
 import app from "./app";
+import config from "./config";
+import { prisma } from "./lib/prisma";
+import "dotenv/config";
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT;
+const PORT = config.port;
 
 async function main(){
     try {
+        await prisma.$connect();
+        console.log("Connected to the database successfully");
+
         app.listen(PORT, ()=>{
             console.log(`Server is running on port ${PORT}`)
         })
     } catch (error) {
         console.error("Error starting the server:", error);
+        await prisma.$disconnect();
+
         process.exit(1);
     }
 }
