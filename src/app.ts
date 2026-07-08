3b5751409e058a066d1728c08fd3220652fba28e
Provash <provash20cb@gmail.com>
2026-07-09 02:33:15 +0600

    Register create

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,65 @@ import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import config from "./config";
 import cookieParser from "cookie-parser";
+import httpStatus from "http-status";
+import { prisma } from "./lib/prisma";
+import bcrypt from "bcryptjs";
 
 const app: Application = express();
 
-app.use(cors({
-    origin : config.app_url,
-    credentials : true
-}));
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/",(req: Request, res: Response)=>{
-    res.send("GearUp is active !!!")
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("GearUp is active !!!");
+});
+
+app.post("/api/users/register", async (req: Request, res: Response) => {
+  // const payload = req.body;
+  // console.log(payload);
+  const { name, email, password, role, profilePhoto } = req.body;
+
+  const isUserExist = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (isUserExist) {
+    throw new Error("User with The email already exists");
+  }
+
+  const hashedPassword = await bcrypt.hash(
+    password,
+    Number(config.bcrypt_salt_rounds)
+  );
+
+  const createdUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+      role,
+      profilePhoto,
+    },
+    omit: {
+      password: true,
+    },
+  });
+
+  res.status(httpStatus.CREATED).json({
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: "User Registered Successfully",
+    data: {
+      user: createdUser,
+    },
+  });
+});
 
 export default app;
