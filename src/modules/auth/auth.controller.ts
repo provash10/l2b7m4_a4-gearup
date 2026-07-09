3a2724b0a1d9e6c2600817b5c1fbdeed1b608a7b
Provash <provash20cb@gmail.com>
2026-07-09 23:46:26 +0600

    Create catchAsync , sendResponse

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,34 +1,50 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { prisma } from "../../lib/prisma";
 import bcrypt from "bcryptjs";
 import config from "../../config";
 import httpStatus from "http-status";
 import { authService } from "./auth.service";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
-const registerUser = async (req: Request, res: Response) => {
-  try {
-    const payload = req.body;
-  // console.log(payload);
- const result = await authService.registerUserIntoDB(payload)
+// const registerUser = async (req: Request, res: Response) => {
+//   try {
+//     const payload = req.body;
+//   // console.log(payload);
+//  const result = await authService.registerUserIntoDB(payload)
 
   
-  res.status(httpStatus.CREATED).json({
-    success: true,
-    statusCode: httpStatus.CREATED,
-    message: "User Registered Successfully",
-    data: result,
-  });
-  } catch (error) {
-    console.log(error);
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-        success : false,
-        statusCode : httpStatus.INTERNAL_SERVER_ERROR,
-        message: "Faild To Register User",
-        error:(error as Error).message
+//   res.status(httpStatus.CREATED).json({
+//     success: true,
+//     statusCode: httpStatus.CREATED,
+//     message: "User Registered Successfully",
+//     data: result,
+//   });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+//         success : false,
+//         statusCode : httpStatus.INTERNAL_SERVER_ERROR,
+//         message: "Faild To Register User",
+//         error:(error as Error).message
 
-    })
+//     })
+//   }
+// }
+
+const registerUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const payload = req.body;
+    const result = await authService.registerUserIntoDB(payload);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User registered successfully",
+      data: result,
+    });
   }
-}
+)
 
 export const userController ={
     registerUser
